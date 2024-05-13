d17f017722b6c0c0d8fc3bfd832afda3cf631dba
fdlnicolau <149716240+fdlnicolau@users.noreply.github.com>
2024-05-13 16:05:50 +0200

    Add files via upload

---
+++ push_swap/src/swap.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lnicolau <lnicolau@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 16:17:45 by lnicolau          #+#    #+#             */
+/*   Updated: 2024/05/09 12:46:27 by lnicolau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	swap_a(t_stack **stack_a, int flag)
+{
+	swap(stack_a);
+	if(flag == 1)
+		ft_printf("sa\n");
+}
+void swap(t_stack **stack)
+{
+	t_stack	*first;
+	t_stack *second;
+	if(stack_size(*stack) > 1)
+	{
+		first = *stack;
+		second = first->next;
+		first->next = second->next;
+		second->next = first;
+		*stack = second;
+	}
+}
